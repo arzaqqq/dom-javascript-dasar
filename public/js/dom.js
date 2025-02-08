462ae93997335d8f9b9d660e4b37d696031d3e74
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-02-08 15:37:25 +0700

    UPDATE: DOM Selector dengan getelementbytagelement

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,2 +1,21 @@
 const title = document.getElementById('title');
 title.style.color = 'red';
+
+
+// 2 : getelementbytagname
+const tagname = document.getElementsByTagName('li');
+// caar 1
+tagname[0].style.color = 'blue';
+tagname[1].style.color = 'green';
+tagname[2].style.fontFamily = 'cursive';
+
+// cara 2 denag nperlulangan
+for (let i = 0 ; i < tagname.length ; i++) {
+    const element = tagname[i];
+    element.style.color = 'red';
+}
+
+// cara 3 for of
+for (const element of tagname) {
+    element.style.color = 'gray';
+}
