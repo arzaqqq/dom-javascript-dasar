eb3e460dd8772435a3dbab3338cc1d2c382b07f7
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-02-09 18:18:10 +0700

    ADD: DOM event lestener dengan onclick dan addeventlistener

---
+++ public/js/eventHandler.js
@@ -0,0 +1,27 @@
+
+
+// CARA 1 PANGGIL
+function showalert()
+{
+    alert('ini alert button1');
+}
+function showalert2()
+{
+    alert('ini alert button 2');
+}
+
+
+document.getElementById("button1").onclick = showalert;
+document.getElementById("button1").onclick = showalert2 ;
+
+
+// CARA 2 PANGGIL
+document.getElementById("button2").addEventListener("click",showalert); 
+document.getElementById("button2").addEventListener("click",showalert2); 
+
+
+/* 
+cara panggil dengan addeventlistener lebik vaik daripada onclick 
+karena jika dengan onclick bis ahapus function lasain ,tapijia addeventlistener tidak dihapus
+
+*/
