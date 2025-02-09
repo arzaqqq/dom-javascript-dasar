4bffca83694a21e1d3d5d795cc401b86cd8f0459
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-02-09 17:53:43 +0700

    UPDATE:Append dan appendchild

--- public/js/manipulation.js
+++ public/js/manipulation.js
@@ -19,3 +19,25 @@ const list  = document.querySelector('.item-class');
 list.classList.add('background');
 list.classList.add('border');
 list.classList.remove('background');
+
+
+// Appendchild
+const item = document.getElementById('item');
+const li = document.createElement('li');
+li.innerText = 'Item 4';
+item.appendChild(li);   
+
+
+// append
+const item2 = document.getElementById('item');
+const li2 = document.createElement('li');
+const li3 = document.createElement('li');
+
+li2.innerText = 'Item 5';
+li3.innerText = 'Item 6';
+
+item2.prepend(li3);//dibgaian depan atau atas
+item2.append(li2);//dibaguian akhir atau bawah
+
+
+// Harus belaajr lagi saat video eps 04
